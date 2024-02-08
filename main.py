e0882e6ea493c2f5ffe05b333293ce438f991ac1
developerpedram <developer.pedram@gmail.com>
2024-02-08 21:26:08 +0330

    Get Html code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import re
+import requests
+
+
+response = requests.get('https://shahreketabonline.com/Products/Category/199/%D8%AF%D8%A7%D8%B3%D8%AA%D8%A7%D9%86-%D8%A7%DB%8C%D8%B1%D8%A7%D9%86')
+
+
+print(response.text)
